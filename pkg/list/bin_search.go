305e0ef1bd785ea971b22e3651215a74011c8d28
shuvava <shuvava@users.noreply.github.com>
2022-09-26 07:15:53 -0700

    add binary insert func

--- pkg/list/bin_search.go
+++ pkg/list/bin_search.go
@@ -1,22 +1,60 @@
 package list
 
-import "fmt"
-
 // BinSearch implementation of binary search on golang
 // this func return -1 with error if target is not found
-func BinSearch[K int | int64 | uint | byte](a []K, target K) (int, error) {
+func BinSearch[K int | int64 | uint | byte](a []K, target K) (int, bool) {
 	l := 0
 	h := len(a)
 	for h > l {
 		m := (h + l) >> 1
-		if a[m] == target {
-			return m, nil
+		if a[m] < target {
+			l = m + 1
+		} else {
+			h = m
 		}
-		if a[m] > target {
+	}
+	return l, l < len(a) && a[l] == target
+}
+
+// BinSelectLeft returns the index where to insert item target in list a,
+// assuming a is sorted.
+//
+//	The return value i is such that all e in a[:i] have e < target, and all e in
+//
+// a[i:] have e >= target.
+//
+//	So if target already appears in the list, a.insert(i, target) will
+//
+// insert just before the leftmost x already there.
+func BinSelectLeft[K int | int64 | uint | byte](a []K, target K) int {
+	l := 0
+	h := len(a)
+	for l < h {
+		m := (h + l) >> 1
+		if a[m] < target {
+			l = m + 1
+		} else {
+			h = m
+		}
+	}
+	return l
+}
+
+// BinSelectRight returns the index where to insert item target in list a, assuming a is sorted.
+//
+//	The return value i is such that all e in a[:i] have e <= target, and all e in
+//	a[i:] have e > target.  So if x already appears in the list, a.insert(i, target) will
+//	insert just after the rightmost x already there.
+func BinSelectRight[K int | int64 | uint | byte](a []K, target K) int {
+	l := 0
+	h := len(a)
+	for l < h {
+		m := (h + l) >> 1
+		if target < a[m] {
 			h = m
 		} else {
 			l = m + 1
 		}
 	}
-	return -1, fmt.Errorf("target value (%d) not found", target)
+	return l
 }
--- pkg/list/bin_search_test.go
+++ pkg/list/bin_search_test.go
@@ -16,9 +16,9 @@ type TestCase struct {
 
 func TestBinarySearch(t *testing.T) {
 	testCases := []TestCase{
-		{"even size middle target Not Fond", []int{1, 3, 5, 6}, 2, -1},
-		{"even size most right target Not Fond", []int{1, 3, 5, 6}, 9, -1},
-		{"even size most left target Not Fond", []int{1, 3, 5, 6}, 0, -1},
+		{"even size middle target Not Fond", []int{1, 3, 5, 6}, 2, 1},
+		{"even size most right target Not Fond", []int{1, 3, 5, 6}, 9, 4},
+		{"even size most left target Not Fond", []int{1, 3, 5, 6}, 0, 0},
 		{"even size most right target", []int{1, 3, 5, 9}, 9, 3},
 		{"even size most left target", []int{1, 3, 5, 9}, 1, 0},
 		{"even size middle target #1", []int{1, 3, 5, 9}, 5, 2},
@@ -26,35 +26,73 @@ func TestBinarySearch(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			r, err := list.BinSearch(tc.list, tc.target)
-			if tc.expected >= 0 && r != tc.expected {
+			r, found := list.BinSearch(tc.list, tc.target)
+			if r != tc.expected {
 				t.Errorf("got %d, expected %d", r, tc.expected)
 			}
-			if tc.expected == -1 && err == nil {
-				t.Errorf("get %d, expected NOT nil error", r)
-			} else if tc.expected >= 0 && err != nil {
-				t.Errorf("get %s, expected nil", err)
+			if found && tc.list[r] != tc.target {
+				t.Errorf("for target:%d looking up in  array[%v] expected %d got %d", tc.target, tc.list, tc.expected, r)
 			}
 		})
 	}
 }
 
-func FuzzBinSearch(f *testing.F) {
+func TestBinSelectLeft(t *testing.T) {
+	testCases := []TestCase{
+		{"even size middle target Not Fond", []int{1, 3, 5, 6}, 2, 1},
+		{"even size most right target Not Fond", []int{1, 3, 5, 6}, 9, 4},
+		{"even size most left target Not Fond", []int{1, 3, 5, 6}, 0, 0},
+		{"even size most right target", []int{1, 3, 5, 9}, 9, 3},
+		{"even size most left target", []int{1, 3, 5, 9}, 1, 0},
+		{"even size middle target #1", []int{1, 3, 5, 9}, 5, 2},
+		{"even size middle target #2", []int{1, 3, 5, 9}, 3, 1},
+		{"should return left most index", []int{0, 1, 1, 1, 2}, 1, 1},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := list.BinSelectLeft(tc.list, tc.target)
+			if r != tc.expected {
+				t.Errorf("got %d, expected %d", r, tc.expected)
+			}
+		})
+	}
+}
+
+func FuzzBinSelectLeft(f *testing.F) {
 	f.Fuzz(func(t *testing.T, a []byte, target byte) {
 		aa := make([]int, len(a))
 		for i, v := range a {
 			aa[i] = int(v)
 		}
 		sort.Ints(aa)
-		expected := sort.SearchInts(aa, int(target))
-		r, err := list.BinSearch(a, target)
-		if a[expected] == target && r != expected {
-			t.Errorf("got %d, expected %d", r, expected)
+		for i, v := range aa {
+			a[i] = byte(v)
 		}
-		if a[expected] != target && err == nil {
-			t.Errorf("get %d, expected NOT nil error", r)
-		} else if a[expected] == target && err != nil {
-			t.Errorf("get %s, expected nil", err)
+		expected := sort.SearchInts(aa, int(target))
+		r := list.BinSelectLeft(a, target)
+		if r != expected {
+			t.Errorf("for target:%d looking up in  array[%v] expected %d got %d", target, a, expected, r)
 		}
 	})
+}
+
+func TestBinSelectRight(t *testing.T) {
+	testCases := []TestCase{
+		{"even size middle target Not Fond", []int{1, 3, 5, 6}, 2, 1},
+		{"even size most right target Not Fond", []int{1, 3, 5, 6}, 9, 4},
+		{"even size most left target Not Fond", []int{1, 3, 5, 6}, 0, 0},
+		{"even size most right target", []int{1, 3, 5, 9}, 9, 4},
+		{"even size most left target", []int{1, 3, 5, 9}, 1, 1},
+		{"even size middle target #1", []int{1, 3, 5, 9}, 5, 3},
+		{"even size middle target #2", []int{1, 3, 5, 9}, 3, 2},
+		{"should return left most index", []int{0, 1, 1, 1, 2}, 1, 4},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := list.BinSelectRight(tc.list, tc.target)
+			if r != tc.expected {
+				t.Errorf("got %d, expected %d", r, tc.expected)
+			}
+		})
+	}
 }
